004192ca1681f52be1ead522947557381b4b8587
Ko-Ying <pengkoying@gmail.com>
2014-11-09 04:07:48 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/Ko-Ying/Desktop")
+ECP<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
+ECP$Date=strptime(paste(ECP$Date,ECP$Time),"%d/%m/%Y %H:%M:%S")
+ECP1=subset(ECP,(ECP$Date>=strptime("2007-02-01","%Y-%m-%d")&(ECP$Date<strptime("2007-02-03","%Y-%m-%d"))))
+library(datasets)
+png(filename="plot2.png",width=480,height=480,bg="white")
+plot(ECP1$Date, ECP1$Global_active_power,type="l",main="Global Active Power",xlab="",ylab="Global Active Power (Kilowatts)")
+dev.off()
